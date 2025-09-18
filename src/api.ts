d20c4ff967755a7c4d185d9530e056f64e089053
Work_station <Qq11221122Qq2@proton.me>
2025-09-18 13:44:18 +0000

    Update API baseURL to deployed backend

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://flower-delivery-y3lj.onrender.com/",
+  baseURL: "https://flower-delivery-y3lj.onrender.com", // <-- твой бекенд URL
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
